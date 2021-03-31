1f777bf8b9a007af6214f9c122ceab783c74aa41
TikkaMasala1 <74255354+TikkaMasala1@users.noreply.github.com>
2021-03-31 16:19:38 +0200

    documentatie bijgevoegd en code wat opgeschoond

--- Mdb_to_postgres.py
+++ Mdb_to_postgres.py
@@ -1,11 +1,8 @@
 from pymongo import MongoClient
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-from psycopg2 import sql
-from psycopg2.extras import execute_values
 
-from time import time
-
+# Intialiseert de databaseverbinding met Postgres
 postgresConnection = psycopg2.connect(user="postgres",
                                       password="root",
                                       host="127.0.0.1",
@@ -14,26 +11,37 @@ postgresConnection = psycopg2.connect(user="postgres",
 postgresConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = postgresConnection.cursor()
 
+# Intialiseert de databaseverbinding met MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 db = client["huwebshop"]
 col = db["products"]
 
 
+# Zet de gegevens van de mongo database om in een list van dictonary's
 def get_products_mongo():
     products_array = []
     data_raw = col.find({'category': {"$ne": None}}, {'_id': 1, 'name': 1,
                                                       'category': 1, 'sub_category': 1, 'sub_sub_category': 1,
                                                       'price': {'selling_price': 1}
                                                       })
+
     for data in data_raw:
+        # Sommige "sub_category" en "sub_sub_category" velden bevatten gegevens die fouten veroorzaken bij het invoegen
+        # in Postgres. Dit filtert de gegevens en vervangt ze met "None"
         if 'sub_category' not in data:
             data['sub_category'] = None
         if 'sub_sub_category' not in data:
             data['sub_sub_category'] = None
+
+        # Het veld "price" is een nested dictionary, dit haalt de gegevens uit de nested dictionary.
+        if 'price' in data:
+            for (key, value) in data['price'].items():
+                data['price'] = value
         products_array.append(data)
     return products_array
 
 
+# Verwijderd de table, dit is voor testing doeleinde
 def delete_table_products():
     cur.execute("""
         DROP TABLE if exists PRODUCTS;    
@@ -41,18 +49,22 @@ def delete_table_products():
     print("Table deleted successfully")
 
 
+# Maakt de benodigde tabel aan
 def create_table_products():
     cur.execute("""
-        CREATE TABLE if not exists PRODUCTS (product_id varchar PRIMARY KEY, category varchar, product_name varchar,
-        sub_category varchar , sub_sub_category varchar);
+        CREATE TABLE if not exists PRODUCTS (product_id varchar PRIMARY KEY,product_name varchar, price int,
+        category varchar, sub_category varchar, sub_sub_category varchar);
         """)
     print("Table created successfully")
 
 
+# Insert de list van dictionary's in de postgres database
+def data_transfer():
+    cur.executemany("""INSERT INTO PRODUCTS(product_id,product_name,category,sub_category,sub_sub_category,price)
+    VALUES (%(_id)s,%(name)s,%(category)s,%(sub_category)s,%(sub_sub_category)s,%(price)s)""", products_data)
+
+
+products_data = get_products_mongo()
 delete_table_products()
 create_table_products()
-products_data = get_products_mongo()
-
-
-cur.executemany("""INSERT INTO PRODUCTS(product_id,category,product_name,sub_category,sub_sub_category)
-VALUES (%(_id)s,%(category)s,%(name)s,%(sub_category)s,%(sub_sub_category)s)""", products_data)
+data_transfer()
